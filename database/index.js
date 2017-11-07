c03954e1f2d705b8a13c70af818269f300d0eac4
mannycov <manny.cov09@gmail.com>
2017-11-07 15:20:44 -0800

    added authentication and completed writing mongo schemas

--- database/index.js
+++ database/index.js
@@ -1,5 +1,7 @@
 const colors = require('colors');
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
 
 mongoose.connect('mongodb://velvetElvies:velvetElvies01@ds147265.mlab.com:47265/boost');
 const db = mongoose.connection;
@@ -12,19 +14,47 @@ db.on('open', () => {
   console.log( colors.blue('connection success'));
 });
 
-// const userSchema = mongoose.Schema({
-//   name: String,
-//   email: String,
-//   password: String
-// });
+const User = new Schema({
+  name: String,
+  username: String,
+  email: String,
+  password: String,
+  goals: Object
+});
 
-// const userModal = mongoose.model('user', userSchema);
+const Goal = new Schema({
+  dateCreated: String,
+  dateStarted: Number,
+  dateCompleted: Number,
+  name: String,
+  description: String,
+  notes: String,
+  checkIn: Object,
+  milestone: Object,
+  color: String
+});
 
-// var newUser = new userModal({
-//   name: 'Ramya',
-//   email: 'test@gmail.com',
-//   password: 'ramya'
-// });
+const Milestone = new Schema({
+  name: String,
+  date: String,
+  description: String,
+  notes: String
+});
+
+const CheckIn = new Schema({
+  name: String,
+  date: String,
+  description: String,
+  notes: String
+});
+
+const userModel = mongoose.model('user', User);
+
+var newUser = new userModel({
+  name: 'Ramya',
+  email: 'test@gmail.com',
+  password: 'ramya'
+});
 
 // newUser.save(function(err, user){
 //   if(err){
--- server/index.js
+++ server/index.js
@@ -5,18 +5,89 @@ const bodyParser = require('body-parser');
 const colors = require('colors');
 const PORT = 3030;
 const db = require('../database/index.js');
+const session = require('express-session');
+const User = require('../database/index.js');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
-app.get('/', function() {
+app.get('/', (req, res) => {
   res.end();
 });
+
+app.get('/login', (req, res) => {
+  res.end()
+});
+
+app.get('/signup', (req, res) => {
+
+});
+
+app.post('/login', (res, req) => {
+  // authenticate the user
+  let username = req.body.user;
+  let passwordAttempt = req.body.password;
+
+  User.findOne({username: username})
+    .exec(function(err, user) {
+      if (!user) {
+        res.redirect('/login');
+      } else {
+        User.comparePassword(passwordAttempt, user.password, function(err, match) {
+          if (match) {
+            // create a session
+            req.session.regenerate(function() {
+              req.session.user = user;
+              res.redirect('/');
+            });
+          } else {
+            console.log('Incorrect password!');
+            res.redirect('/login');
+          }
+        });
+      }
+    });
+});
+
+app.post('/signup', (req, res) => {
+  // store the username and password from the request body
+  let usernmae = req.body.username;
+  let password = req.body.password;
+
+  // check if the user already exists
+  User.findOne({username: username})
+    .exec(function(err, user) {
+      if (!user) {
+        // create a password and encrypt it
+        // set username and password
+        bcrypt.genSalt(10, function(err, salt) {
+          bcrypt.hash(password, salt, function(err, hash) {
+              let newUser = new User({
+                username: username,
+                password: hash
+              });
+              // save the user to the db
+              newUser.save()
+                .then(function(user) {
+                  console.log('Successfully saved the user to the database!');
+                  // create a session for the user
+                  req.session.regenerate(function() {
+                    req.session.user = user;
+                    res.redirect('/');
+                  });
+                });
+          });
+        });
+      } else {
+        console.log('This account already exists!');
+        res.redirect('/signup');
+      }
+    });
+});
+
 
 app.listen(PORT, () => {
   console.log(colors.blue(`Listening on PORT ${PORT}`));
 });
-
-
-
